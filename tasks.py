386e9555b7de1a37e5955164f37acdf3be46406d
Mirko Rossini <mrossini@tower-research.com>
2016-10-13 15:56:41 -0400

    Added coverage
    Change-Id: I8acce3b2ef91ca8895648b3fc84870513e921b2c

--- tasks.py
+++ tasks.py
@@ -4,11 +4,18 @@
 from invoke import task
 
 @task
+def install_build_dependencies(ctx):
+    ctx.run('pip install -r requirements_build.txt')
+
+@task
 def test(ctx):
-    ctx.run('pytest')
+    ctx.run('pytest --cov zanna')
 
+@task
+def validate(ctx):
+    ctx.run('flake8 zanna')
 
-@task(test)
+@task(test, validate)
 def build(ctx):
     pass
     
