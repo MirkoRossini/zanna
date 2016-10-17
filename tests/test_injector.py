295419f877b2cd9a0e529ace488af43b4278bead
Mirko Rossini <mrossini@tower-research.com>
2016-10-17 17:56:44 -0400

    Added test for get_instance in injector
    Change-Id: I3e9c66897b14a45a4f80e2e98dee7d19cad010dd

--- tests/test_injector.py
+++ tests/test_injector.py
@@ -13,6 +13,15 @@ class TestBindings(unittest.TestCase):
         module.side_effect = self._called_with_bindings
         Injector(module)
         assert module.called
+    
+    def test_get_instance(self):
+        i = Injector(lambda bindings: bindings.bind_to("thing", 3))
+        class ThingConsumer:
+            def __init__(self, thing):
+                assert thing == 3
+        thing_consumer = i.get_instance(ThingConsumer)
+        assert thing_consumer is not None
+        assert isinstance(thing_consumer, ThingConsumer)
 
     @staticmethod
     def _called_with_bindings(bindings):
--- zanna/_injector.py
+++ zanna/_injector.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Union
 from ._default_bindings import _DefaultBindings
 
 
@@ -6,3 +6,7 @@ class Injector(object):
     def __init__(self, module: Callable, *othermodules: Iterable[Callable]):
         self._bindings = _DefaultBindings()
         module(self._bindings)
+
+    def get_instance(self, string_or_class: Union[type, str]) -> Any:
+        return None
+        
