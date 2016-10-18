a39d9353630f669e8daa3d84ae3472ff45af29b2
Mirko Rossini <mrossini@tower-research.com>
2016-10-18 17:48:58 -0400

    Now using different classes for binding specs
    Change-Id: Ib4ca62571d497ba82edc1f69bd63ccfb6930cd3d

--- tests/test_binding_spec.py
+++ tests/test_binding_spec.py
@@ -1,25 +1,25 @@
 
 from unittest import TestCase
-from zanna._binding_spec import BindingSpec
+from zanna._binding_spec import InstanceBindingSpec
 
 class DummyClass(object):
     def __init__(self, value):
         self.value = value
 
 
-class TestBindingSpect(TestCase):
+class TestInstanceBindingSpec(TestCase):
     TEST_STRING = "testtest"
     TEST_INT = 9999
     TEST_DUMMY_INSTANCE = DummyClass(TEST_STRING)
 
     def test_instance_binding_spec(self):
-        bspec = BindingSpec(self.TEST_STRING)
+        bspec = InstanceBindingSpec(self.TEST_STRING)
         assert bspec.get_instance() == self.TEST_STRING
 
     def test_int_instance_binding_spec(self):
-        bspec = BindingSpec(self.TEST_INT)
+        bspec = InstanceBindingSpec(self.TEST_INT)
         assert bspec.get_instance() == self.TEST_INT
 
     def test_dummyclass_instance_binding_spec(self):
-        bspec = BindingSpec(self.TEST_DUMMY_INSTANCE)
+        bspec = InstanceBindingSpec(self.TEST_DUMMY_INSTANCE)
         assert bspec.get_instance() == self.TEST_DUMMY_INSTANCE
--- zanna/_binding_spec.py
+++ zanna/_binding_spec.py
@@ -1,21 +1,46 @@
 
-from typing import Any
+from typing import Any, Dict
 from inspect import isfunction, isclass
+import abc
 
+def _needs_spec(bound_object: Any) -> bool:
+    return isclass(bound_object) or isfunction(bound_object)
 
-class BindingSpec(object):
+class BindingSpec(metaclass=abc.ABCMeta):
+
+    def has_instance(self) -> bool:
+        """
+        Returns true if the binding spec have an 
+        instance ready to be returned, False otherwise.
+        """
+        raise NotImplementedError()
+
+    def get_instance(self) -> Any:
+        """
+        Returns the instance associated with the BindingSpec.
+        If the instance is not ready (i.e. it's None)
+        it raises a 
+        """
+        raise NotImplementedError()
+    
+    def construct_instance(keyword_arguments: Dict[str, object]) -> Any:
+        """
+        Constructs an instance of the bound object given a map of arguments
+        """
+        pass
+
+class InstanceBindingSpec(BindingSpec):
     def __init__(self, bound_object: Any):
         self._spec = None
-        if self._needs_spec(bound_object):
-            self._instance = None
-            raise Exception()
+        if _needs_spec(bound_object):
+            raise TypeError(
+                "{} should only be used with object instances.".format(
+                    self.__class__.__name__))
         else:
             self._instance = bound_object
 
     def get_instance(self):
         return self._instance
             
-    def _needs_spec(self, bound_object: Any) -> bool:
-        return isclass(bound_object) or isfunction(bound_object)
 
 
