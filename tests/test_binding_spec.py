943418f4aaf75890ae462a28e602f5afc34cf92e
Mirko Rossini <mrossini@tower-research.com>
2016-10-18 17:51:38 -0400

    InstanceBindingSpec now does type check
    Change-Id: Iadb7b54782c678ee3e983a3d58cc6b81d514ab31

--- tests/test_binding_spec.py
+++ tests/test_binding_spec.py
@@ -1,6 +1,7 @@
 
 from unittest import TestCase
 from zanna._binding_spec import InstanceBindingSpec
+import pytest 
 
 class DummyClass(object):
     def __init__(self, value):
@@ -11,6 +12,7 @@ class TestInstanceBindingSpec(TestCase):
     TEST_STRING = "testtest"
     TEST_INT = 9999
     TEST_DUMMY_INSTANCE = DummyClass(TEST_STRING)
+    TEST_CALLABLE = lambda x: x + 500
 
     def test_instance_binding_spec(self):
         bspec = InstanceBindingSpec(self.TEST_STRING)
@@ -23,3 +25,7 @@ class TestInstanceBindingSpec(TestCase):
     def test_dummyclass_instance_binding_spec(self):
         bspec = InstanceBindingSpec(self.TEST_DUMMY_INSTANCE)
         assert bspec.get_instance() == self.TEST_DUMMY_INSTANCE
+
+    def test_class_binding_spec_raises(self):
+        with pytest.raises(TypeError):
+            bspec = InstanceBindingSpec(DummyClass)
