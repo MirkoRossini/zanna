cef4aa412230fc0a564aed91c6c84f7fd330732b
Mirko Rossini <mrossini@tower-research.com>
2016-10-18 17:36:24 -0400

    more tests for bindings
    Change-Id: I036b1637e6ecbe88cd9e5eaf4c55ed0335a9effd

--- tests/test_binding_spec.py
+++ tests/test_binding_spec.py
@@ -2,10 +2,24 @@
 from unittest import TestCase
 from zanna._binding_spec import BindingSpec
 
+class DummyClass(object):
+    def __init__(self, value):
+        self.value = value
+
 
 class TestBindingSpect(TestCase):
     TEST_STRING = "testtest"
+    TEST_INT = 9999
+    TEST_DUMMY_INSTANCE = DummyClass(TEST_STRING)
+
     def test_instance_binding_spec(self):
-        
         bspec = BindingSpec(self.TEST_STRING)
         assert bspec.get_instance() == self.TEST_STRING
+
+    def test_int_instance_binding_spec(self):
+        bspec = BindingSpec(self.TEST_INT)
+        assert bspec.get_instance() == self.TEST_INT
+
+    def test_dummyclass_instance_binding_spec(self):
+        bspec = BindingSpec(self.TEST_DUMMY_INSTANCE)
+        assert bspec.get_instance() == self.TEST_DUMMY_INSTANCE
