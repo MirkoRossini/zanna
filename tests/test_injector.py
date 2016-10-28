656bf959360899c632b6df8ab012f0a736c86fc2
Mirko Rossini <mrossini@tower-research.com>
2016-10-28 03:09:43 -0400

    Implemented injection
    Change-Id: Id50f70744789c98839b34f5285fa268a381eb1c3

--- tests/test_injector.py
+++ tests/test_injector.py
@@ -5,6 +5,16 @@ from zanna import Injector
 from zanna import Binder
 
 
+THING_VALUE = 3
+
+class ThingConsumer:
+    def __init__(self, thing):
+        assert thing == THING_VALUE
+class JustAClass:
+    pass
+
+
+
 class TestBinder(unittest.TestCase):
     def test_init_empty(self):
         self.assertRaises(TypeError, Injector)
@@ -16,15 +26,25 @@ class TestBinder(unittest.TestCase):
         assert module.called
 
     def test_get_simple_instance(self):
-        i = Injector(lambda binder: binder.bind_to("thing", 3))
+        i = Injector(lambda binder: binder.bind_to("thing", THING_VALUE))
         thing = i.get_instance("thing")
-        assert thing == 3
+        assert thing == THING_VALUE
 
-    def test_get_instance(self):
-        i = Injector(lambda binder: binder.bind_to("thing", 3))
-        class ThingConsumer:
-            def __init__(self, thing):
-                assert thing == 3
+    def test_get_instance_of_class(self):
+        i = Injector(lambda binder: binder.bind(JustAClass))
+        instance = i.get_instance(JustAClass)
+        assert instance is not None
+        assert isinstance(instance, JustAClass)
+
+    def test_get_instance_of_class_with_missing_arg_raises(self):
+        i = Injector(lambda binder: binder.bind(ThingConsumer))
+        self.assertRaises(ValueError, i.get_instance, ThingConsumer)
+
+    def test_get_instance_of_class_with_arg(self):
+        def module(binder):
+            binder.bind(ThingConsumer)
+            binder.bind_to("thing", THING_VALUE)
+        i = Injector(module)
         thing_consumer = i.get_instance(ThingConsumer)
         assert thing_consumer is not None
         assert isinstance(thing_consumer, ThingConsumer)
--- zanna/_injector.py
+++ zanna/_injector.py
@@ -11,6 +11,17 @@ class Injector(object):
         binding_spec = self._binder.get_binding(string_or_class)
         if binding_spec.has_instance():
             return binding_spec.get_instance()
-        
-        return None
-        
+        else:
+            return self._construct_instance(binding_spec)
+
+    def _construct_instance(self, binding_spec):
+        argument_specs = binding_spec.get_argument_specs()
+        return binding_spec.construct_instance({arg.name: self._get_instance_for_argument_spec(arg) for arg in argument_specs})
+
+    def _get_instance_for_argument_spec(self, argument_spec):
+        if argument_spec.type is None:
+            return self.get_instance(argument_spec.name)
+        try:
+            return self.get_instance(argument_spec.type)
+        except ValueError:
+            return self.get_instance(argument_spec.name)
